b66fb08a50630314436d2fa81690c5f04b129685
David Poole <14233215+davidlpoole@users.noreply.github.com>
2022-01-16 11:41:44 +1300

    refactor Persons component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ query {
 }
 `
 
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   const result = useQuery(ALL_PERSONS)
 
@@ -20,7 +33,7 @@ const App = () => {
 
   return (
     <div>
-      {result.data.allPersons.map(p => p.name).join(', ')}
+      <Persons persons={result.data.allPersons} />
     </div>
   )
 }
