76292a35e08282401aeb5e8fd02aba374215cd0e
David Poole <14233215+davidlpoole@users.noreply.github.com>
2022-01-16 14:33:15 +1300

    update cache and display mutation errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
@@ -6,6 +6,7 @@ import { ALL_PERSONS } from './queries.js'
 
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
@@ -14,10 +15,29 @@ const App = () => {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
+    </div>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return (
+    <div style={{ color: 'red' }}>
+      {errorMessage}
     </div>
   )
 }
