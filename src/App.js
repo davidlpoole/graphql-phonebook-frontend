b5ae87b2b48ff7e6fa1e51c07c83955783b01333
David Poole <14233215+davidlpoole@users.noreply.github.com>
2022-01-16 14:00:59 +1300

    add new person and refactor queries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries.js'
 
-const ALL_PERSONS = gql`
-{
-  allPersons  {
-    name
-    phone
-    id
-  }
-}
-`
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS)
+  const result = useQuery(ALL_PERSONS, {
+    pollInterval: 2000
+  })
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
   return (
-    <Persons persons={result.data.allPersons} />
+    <div>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm />
+    </div>
   )
 }
 
